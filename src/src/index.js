8ff64615a94eea7246d9a0b7e4a32e2e665c9121
Dorian Amouroux <dor.amouroux@gmail.com>
2018-03-22 14:53:34 +0100

    get the data to display from an endpoint instead of hardcoded

--- src/src/index.js
+++ src/src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  window.DATA_CI = {
-    'deleted': {
-      'product': 'examples/old.png',
-    },
-    'modified': {
-      'services': [
-        'examples/old.png',
-        'examples/diff.png',
-        'examples/new.png',
-      ],
-    },
-    'created': { 'home': 'examples/new.png', }
-  }
-}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cleanPath = window.location.pathname.replace(/^\/|\/$/g, '');
+const urlData = `${process.env.REACT_APP_S3_URL}/${cleanPath}.json`
+
+
+fetch(urlData)
+  .then(data => data.json())
+  .then(data => {
+    window.DATA_CI = data
+    ReactDOM.render(<App />, document.getElementById('root'));
+  })
+  .catch(error => {
+    ReactDOM.render(<div>Not found</div>, document.getElementById('root'));
+  })
